526935f32192a06c4cf5ece5e70cde43faaa18ce
Matt Howe <mrflips@users.noreply.github.com>
2018-07-27 10:19:08 -0400

    Create ckl-append.ps1

---
+++ ckl-append.ps1
@@ -0,0 +1,95 @@
+param([String]$SourceChecklist,[String]$DestinationChecklist);
+
+Write-Host ""
+
+if ([String]::IsNullOrEmpty($SourceChecklist))
+{
+        write-host -ForegroundColor Red "Source checklist must be specified";
+        write-host -ForegroundColor Red "Exiting Script";
+        exit 1;
+}
+
+if ([String]::IsNullOrEmpty($DestinationChecklist))
+{
+        write-host -ForegroundColor Red "Destination checklist must be specified";
+        write-host -ForegroundColor Red "Exiting Script";
+        exit 1;
+}
+
+If (Test-Path $SourceChecklist)
+{
+		Write-Host "Found source checklist."
+		Write-Host ""
+}Else{
+        write-host -ForegroundColor Red "Source checklist must be specified";
+        write-host -ForegroundColor Red "Exiting Script";
+        exit 1;
+}
+
+If (Test-Path $DestinationChecklist)
+{
+		Write-Host "Found destination checklist."
+		Write-Host ""
+}Else{
+        write-host -ForegroundColor Red "Destination checklist must be specified";
+        write-host -ForegroundColor Red "Exiting Script";
+        exit 1;
+}
+
+Write-Host "Content of source checklist being transferred."
+Write-Host ""
+
+[xml]$SourceSTIGChecklistData = Get-Content $SourceChecklist
+$DestinationSTIGChecklistData = New-Object xml
+$DestinationSTIGChecklistData.psbase.PreserveWhitespace = $true
+$DestinationSTIGChecklistData.Load($DestinationChecklist)
+
+[System.Xml.XmlElement] $srcchecklist = $SourceSTIGChecklistData.CHECKLIST
+[System.Xml.XmlElement] $destchecklist = $DestinationSTIGChecklistData.CHECKLIST
+
+foreach($vuln in $srcchecklist.STIGS.iSTIG.ChildNodes)
+{
+	foreach($stigdata in $vuln.ChildNodes)
+	{   
+		If ($stigdata.VULN_ATTRIBUTE -eq "Vuln_Num")
+		{
+			[string] $vulnid = $stigdata.ATTRIBUTE_DATA
+		}
+	}
+	
+	[string] $status = $vuln.STATUS
+	[string] $findingdetails = $vuln.FINDING_DETAILS
+	[string] $comments = $vuln.COMMENTS
+	[string] $severityoverride = $vuln.SEVERITY_OVERRIDE
+	[string] $severityjustification = $vuln.SEVERITY_JUSTIFICATION
+	
+	foreach($destvuln in $destchecklist.STIGS.iSTIG.ChildNodes)
+	{
+		foreach($deststigdata in $destvuln.ChildNodes)
+		{   
+			If ($deststigdata.VULN_ATTRIBUTE -eq "Vuln_Num")
+			{
+				If ($deststigdata.ATTRIBUTE_DATA -eq $vulnid)
+				{
+					#$destvuln.STATUS = $status
+					#$destvuln.FINDING_DETAILS = $findingdetails
+					#$destvuln.COMMENTS = $comments
+					#$destvuln.SEVERITY_OVERRIDE = $severityoverride
+					#$destvuln.SEVERITY_JUSTIFICATION = $severityjustification
+          $destvuln.COMMENTS = $destvuln.COMMENTS + $comments
+					Write-Host "ID = $vulnid STATUS = $status"
+				}
+			}
+		}
+	}
+}
+
+$DestinationSTIGChecklistData.Save($DestinationChecklist)
+
+$MyFile = Get-Content $DestinationChecklist
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+[System.IO.File]::WriteAllLines($DestinationChecklist, $MyFile, $Utf8NoBomEncoding)
+
+Write-Host ""
+Write-Host "Contents of $SourceChecklist have been transferred to $DestinationChecklist."
+Write-Host ""
