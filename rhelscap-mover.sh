074710390af2c5950df233ba6819636ac56bf6fc
Matt Howe <mrflips@users.noreply.github.com>
2018-08-02 19:22:11 -0400

    Update rhelscap-mover.sh

--- rhelscap-mover.sh
+++ rhelscap-mover.sh
@@ -9,3 +9,11 @@ for i in vm1 vm2 vm3 vm4 vm5 ;
     ssh $i "sudo chown -R $user:'$group' /home/$user/SCC/" &&
     scp -r $i:/home/$user/SCC/Results/SCAP/ /tmp/r/ ;
 done
+
+#can combine with above
+for i in vm1 vm2 vm3 vm4 vm5 ;
+    do scp -r /tmp/manual-rhel6.sh $i:/tmp/ &&
+    ssh $i 'sudo bash /tmp/manual-rhel6-new.sh' &&
+    ssh $i 'sudo chmod 750 /tmp/manual-results' &&
+    scp -r $i:/tmp/manual-results /tmp/manual-results-$i ;
+done
