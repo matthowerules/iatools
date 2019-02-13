19120f0e0e2ff93c7e59c248b82b6f3c7fb3d37b
Matt Howe <mrflips@users.noreply.github.com>
2019-02-13 21:11:57 +0000

    Update rhelscap-mover.sh

--- rhelscap-mover.sh
+++ rhelscap-mover.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 user=`whoami`
 group=`id -gn`
+benchmark="/tmp/U_RedHat_6_V1R15_STIG_SCAP_1-1_Benchmark.zip"
 for i in vm1 vm2 vm3 vm4 vm5 ;
-    do scp -r /tmp/U_RedHat_6_V1R15_STIG_SCAP_1-1_Benchmark.zip $i:/tmp/ &&
-    ssh $i 'sudo rm -rf ~/SCC/Results/SCAP/' &&
-    ssh $i "sudo /opt/scc/cscc -isr /tmp/U_RedHat_6_V1R15_STIG_SCAP_1-1_Benchmark.zip" &&
+    do scp -r $benchmark $i:/tmp/ &&
+    ssh $i "sudo rm -rf /home/$user/SCC/Results/SCAP/" &&
+    ssh $i "sudo /opt/scc/cscc -isr $benchmark" &&
     ssh $i "sudo chmod -R 777 /home/$user/SCC/" &&
     ssh $i "sudo chown -R $user:'$group' /home/$user/SCC/" &&
     scp -r $i:/home/$user/SCC/Results/SCAP/ /tmp/r/ ;
