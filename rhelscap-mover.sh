1ba3ab8a76b403f9fbdd15d6c82687d140aafd19
Matt Howe <mrflips@users.noreply.github.com>
2019-02-13 21:03:57 +0000

    Update rhelscap-mover.sh
    works

--- rhelscap-mover.sh
+++ rhelscap-mover.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 user=`whoami`
-###TODO - group=`id -gn`
+group=`id -gn`
 for i in vm1 vm2 vm3 vm4 vm5 ;
     do scp -r /tmp/U_RedHat_6_V1R15_STIG_SCAP_1-1_Benchmark.zip $i:/tmp/ &&
     ssh $i 'sudo rm -rf ~/SCC/Results/SCAP/' &&
-    ssh $i 'sudo /opt/scc/cscc -isr /tmp/U_RedHat_6_V1R15_STIG_SCAP_1-1_Benchmark.zip' &&
-    ssh $i 'sudo chmod -R 777 /home/'$user'/SCC/' &&
-    ssh $i "sudo chown -R $user:'$group' /home/"$user"/SCC/" &&
+    ssh $i "sudo /opt/scc/cscc -isr /tmp/U_RedHat_6_V1R15_STIG_SCAP_1-1_Benchmark.zip" &&
+    ssh $i "sudo chmod -R 777 /home/$user/SCC/" &&
+    ssh $i "sudo chown -R $user:'$group' /home/$user/SCC/" &&
     scp -r $i:/home/$user/SCC/Results/SCAP/ /tmp/r/ ;
 done
 
