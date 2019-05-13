06602d4e653bcdbef732f52538a7cea17fe0f9f9
Matt Howe <mrflips@users.noreply.github.com>
2019-05-13 12:24:40 -0400

    clearing previously installed scap
    ran into an issue where it would lock up otherwise

--- rhelscap-mover.sh
+++ rhelscap-mover.sh
@@ -5,6 +5,7 @@ benchmark="/tmp/U_RedHat_6_V1R15_STIG_SCAP_1-1_Benchmark.zip"
 for i in vm1 vm2 vm3 vm4 vm5 ;
     do scp -r $benchmark $i:/tmp/ &&
     ssh $i "sudo rm -rf /home/$user/SCC/Results/SCAP/" &&
+    ssh $i "sudo /opt/scc/cscc -ua" &&
     ssh $i "sudo /opt/scc/cscc -isr $benchmark" &&
     ssh $i "sudo chmod -R 777 /home/$user/SCC/" &&
     ssh $i "sudo chown -R $user:'$group' /home/$user/SCC/" &&
