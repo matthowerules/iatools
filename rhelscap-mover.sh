56bd007a46afc9dfe7aa177f1e997eb5bdf6f93c
Matt Howe <11624216+matthowerules@users.noreply.github.com>
2019-08-11 10:07:12 -0400

    Update rhelscap-mover.sh

--- rhelscap-mover.sh
+++ rhelscap-mover.sh
@@ -7,7 +7,7 @@ for i in vm1 vm2 vm3 vm4 vm5 ;
     ssh $i "sudo rm -rf /home/$user/SCC/Results/SCAP/" &&
     ssh $i "sudo /opt/scc/cscc -ua" &&
     ssh $i "sudo /opt/scc/cscc -isr $benchmark" &&
-    ssh $i "sudo chmod -R 777 /home/$user/SCC/" &&
+    ssh $i "sudo chmod -R 600 /home/$user/SCC/" &&
     ssh $i "sudo chown -R $user:'$group' /home/$user/SCC/" &&
     scp -r $i:/home/$user/SCC/Results/SCAP/ /tmp/r/ ;
 done
@@ -16,6 +16,6 @@ done
 for i in vm1 vm2 vm3 vm4 vm5 ;
     do scp -r /tmp/man-rhel-v3ip.sh $i:/tmp/ &&
     ssh $i 'sudo bash /tmp/man-rhel-v3ip.sh' &&
-    ssh $i 'sudo chmod 750 /tmp/manual-results' &&
+    ssh $i 'sudo chmod 600 /tmp/manual-results' &&
     scp -r $i:/tmp/manual-results /tmp/manual-results-$i ;
 done
