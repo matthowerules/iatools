589e34cc62aa401c41e703ffb46d0d43b48a8529
Matt Howe <11624216+matthowerules@users.noreply.github.com>
2020-03-10 12:13:00 -0400

    Create init.pp

---
+++ pupmod/adobe-acrobat/init.pp
@@ -0,0 +1,51 @@
+class adobepro {
+  include adobepro::acrgpo
+
+  package{'AdobePro11':
+    ensure   =>absent,
+    provider =>chocolatey,
+  }
+
+  package{'AdobePro':
+    ensure   =>latest,
+    provider =>chocolatey,
+  } -> 
+
+  package{'AdobePro-Patch':
+    ensure   =>latest,
+    provider =>chocolatey,
+  }
+
+  #Load Adobe ADMX GPO Templates
+  file { 'c:\Windows\PolicyDefinitions\AcrobatProDCContinuous.admx':
+    ensure => 'file',
+    mode   => '0660',
+    group  => 'BUILTIN\\Administrators',
+    source => 'puppet:///modules/adobepro/AcrobatProDCContinuous.admx',
+  }
+
+  file { 'c:\Windows\PolicyDefinitions\en-US\AcrobatProDCContinuous.adml':
+    ensure => 'file',
+    mode   => '0660',
+    group  => 'BUILTIN\\Administrators',
+    source => 'puppet:///modules/adobepro/AcrobatProDCContinuous.adml',
+  }
+
+  file{'c:/programdata/patching/acrobatstig':
+    ensure  => absent,
+    force   => true,
+  } 
+
+  file { 'c:\ProgramData\patching\acrobat-stig.ps1':
+    ensure => file,
+    source => 'puppet:///modules/adobepro/acrobat-stig.ps1',
+  }
+
+  exec { 'AcrobatSTIG' :
+    command   =>  'powershell.exe -file "c:\programdata\patching\acrobat-stig.ps1"',
+    unless    =>  'powershell.exe -Command "if ((Get-ItemProperty -path HKLM:\software\dcgs\patches).hkuctacrobat -ne (gci -path registry::hkey_users).count) { exit 1 } else { exit 0 }"',
+    provider  =>  powershell,
+    require   =>  File['c:\ProgramData\patching\acrobat-stig.ps1'],
+    logoutput =>  true,
+  }
+}
